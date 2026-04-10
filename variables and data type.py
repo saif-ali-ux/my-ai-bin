dc656b19eb17346030931ec6b7d7df5e12a0ba22
Saif Ali ux <maliksaifali112i@gmail.com>
2026-04-10 15:59:23 +0500

    Create variables and data type.py

---
+++ variables and data type.py
@@ -0,0 +1,61 @@
+# #Variables AND data type 
+# #Q1: Create integer variable age and a float variables height . Print their types.
+# age=18
+# height=5.11
+# print(type(age))
+# print(type(height))
+
+# #Q2:store the value 3 + 4j in variable.print the variable and its type.
+
+# value="3+4j"
+# print(type(value),value )
+
+# #Q3: create a boolean variables is_python_fun and set it to True.
+
+# is_python_fun = True
+# print(is_python_fun)
+# print(type(is_python_fun)) 
+
+# #Q4:METHOD 1: Assign three different values to three variables in a single line.
+# a=9
+# b=0
+# c=8
+# print(a,b,c)
+
+# #Q5:METHOD 2: Assign the same value  to three different variables in a single line.
+# a=7
+# b=7
+# c=7
+# print(a,b,c)
+# #by ai gimmini 
+# x = y = z = "Python is fun"
+
+# # Let's verify by printing them
+# print(x)
+# print(y)
+# print(z)
+
+# #Q6: Take a numeric input from a user and covert it to a float.
+
+# num=float(input("Enter a number:"))
+# print(type(num)) 
+
+# #Q7: Take a string input"100"and convert it to an int.
+# num1=int(input("Enter an string:"))
+# print(type(num1))
+
+# #Q8: create a variable with a complex number and print only its real part.
+
+# value=5 + 3j
+# print(value.real)
+
+# #Q9: Define a string variable containing a paragraph and print its len.
+# b="I am studing py from apna college\nand its found so much itresting\ni am doing good"
+# print(len(b))
+# print(b)
+
+# #Q10:Swap the values of two variables a and b without using a third variables.  "tuple unpacking."
+# a="fantasy"
+# b="Zebra"
+# a,b=b,a
+# print(a,b)
