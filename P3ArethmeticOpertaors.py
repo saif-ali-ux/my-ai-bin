8ae9337453e36b03fcf97e9a1153cbb7c1473dfe
Saif Ali ux <maliksaifali112i@gmail.com>
2026-04-11 22:09:54 +0500

    Create P3ArethmeticOpertaors.py

---
+++ P3ArethmeticOpertaors.py
@@ -0,0 +1,65 @@
+# #Q1:WAP to to calculate the area of a rectangle (length* Width).
+# length=int(input("Enter your lenght:"))
+# width=int(input("Enter your Width:"))
+# print("Area of rectanle is :", length*width)
+
+# #Q2:Take two numbers and print the result of the first raised to the power of second(a^b).
+# a=int(input("Enter yoyr first number:"))
+# b=int(input("Enter your second number:"))
+# results=a**b
+# print("Results=",results)
+
+# #Q3:demonstrte the difference b/w (divison)and//(floor divison)with the numbers 10 and 3.
+# num1=10
+# num2=3
+# results=num1//num2
+# print(results)
+
+# #Q4:Use the modulus operator % to find the reminder when 25 is devided by 4.
+# num1=25
+# num2=4
+# remainder=num1%num2
+# print(remainder)
+
+# #Q5:Calculate the average of five numbers entered by the user .
+# num1=int(input("Enter your first number:"))
+# num2=int(input("Enter your second number:"))
+# num3=int(input("Enter your third number:"))
+# num4=int(input("Enter your fourth number:"))
+# num5=int(input("Enter your fifth number:"))
+# averge =(num1 + num2 + num3 + num4 + num5)//5 #if we want two find our averge in float just remove one /. and type cast it into float.
+# print("Averge of five numbers is :", averge)
+
+# #Q6:Create a program that converts minutes into hours and remaining minutes.
+
+# total_minutes=int(input("Enter your total minutes:"))
+# hours=total_minutes//60
+# remaining_min=total_minutes%60
+# print(hours,remaining_min)
+
+# #Q7:Calculate the area of a circle where Area = \pi r^2 (Use 3.14 for \pi).
+# r=float(input("Enter your radious:"))
+# area=3.14*r*r
+# print("area of a circle is :",area)
+
+# #Q8:Find the cube of a number entered by the user.
+# num=int(input("Enter your number:"))
+# print(num*3)
+
+# #Q9:Perform the calculation 10 + 5 * 2. Does Python follow PEMDAS? Prove it with code.
+# result = 10 + 5 * 2
+# print("10 + 5 * 2 =", result)
+
+# #Q10:Write a program to calculate simple interest: (P * R * T) / 100.
+# # Simple Interest Calculator
+
+# P = int(input("Enter Principal amount (P): "))
+# R = int(input("Enter Rate of interest (R): "))
+# T = int(input("Enter Time in years (T): "))
+
+# # Calculating Simple Interest
+# SI = (P * R * T) //100
+
+# # Displaying result
+# print("Simple Interest =", SI)
+
