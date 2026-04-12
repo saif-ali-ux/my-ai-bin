2311fbcb59d44cc1fe6d2c902625fcbfa3cb8225
Saif Ali ux <maliksaifali112i@gmail.com>
2026-04-12 20:06:45 +0500

    Create partA,40 total,Lists, Tuples, Sets, Dictionaries.py

---
+++ partA,40 total,Lists, Tuples, Sets, Dictionaries.py
@@ -0,0 +1,58 @@
+# #Part A — Python Lists (10 Beginner Questions)
+# #Q1:Create a list nums = [3, 1, 4, 1, 5] and print the first and last elements.
+# #Tip: Use nums[0] and nums[-1].
+# nums = [3, 1, 4, 1, 5]
+# num=nums[0],[-1]
+# print(num)
+
+# #Q2:. Find the length of the list colors = ['red', 'blue', 'green'].
+# #Tip: Use len(colors).
+# colors = ['red', 'blue', 'green']
+# print(len(colors) ,colors)
+
+# #Q3:Append 'yellow' to the list colors = ['red', 'blue'].
+# #Tip: Use append().
+# colors=['red', 'blue']
+# colors.append('yellow')
+# print(colors)
+
+# #Q4:Insert 'orange' at index 1 in fruits = ['apple', 'banana'].
+# #Tip: Use insert(index, value).
+# fruits = ['apple', 'banana']
+# fruits.insert(1,'orange')
+# print(fruits)
+
+# #Q5:Remove 'banana' from fruits = ['apple', 'banana', 'grapes'].
+# #Tip: Use remove(value).
+# fruits = ['apple', 'banana', 'grapes']
+# fruits.remove('banana')
+# print(fruits)
+
+# #Q6:Pop the last element from items = [10, 20, 30] and print the popped value.
+# #Tip: Call items.pop().
+# items = [10, 20, 30]
+# item=items.pop(2)
+# print(item)
+
+# #Q7:Check if 3 is in the list nums = [1, 2, 3, 4].
+# #Tip: Use the in operator.
+# nums = [1, 2, 3, 4]
+# print(3 in nums)
+
+# #Q8:Print the slice [2, 3] from the list [0, 1, 2, 3, 4].
+# #Tip: Use slicing: a[2:4].
+# l=[0, 1, 2, 3, 4]
+# print(l[2:4])
+
+# #Q9:Replace the element at index 1 in a = [5, 10, 15] with 12.
+# #Tip: Use assignment: a[1] = 12.
+# a = [5, 10, 15]
+# a[1]=12
+# print(a)
+
+# #Q10:Count how many times 2 appears in [1, 2, 2, 3, 2].
+# #Tip: Use list.count(value).
+# l=[1, 2, 2, 3, 2]
+# x=l.count(2)
+# print("number 2 appers in this list are:",x)
+
